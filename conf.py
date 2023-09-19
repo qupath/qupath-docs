03f9e8459814dd139cde21ceed531aef96dc0986
finglis <42358257+finglis@users.noreply.github.com>
2023-09-19 11:46:24 +0100

    minor edit

--- conf.py
+++ conf.py
@@ -86,7 +86,7 @@ myst_enable_extensions = [
     "fieldlist",
     "html_admonition",
     "html_image",
-    "linkify",
+    # "linkify",
     "replacements",
     "smartquotes",
     "strikethrough",
